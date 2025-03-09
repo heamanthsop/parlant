131034587476d62ab4aed096d95733cc67aaf466
Menachem Brichta <menachem@emcie.co>
2025-03-09 11:54:24 +0000

    adjust changes - message fragments
    Signed-off-by: Menachem Brichta <menachem@emcie.co>
    
    Signed-off-by: Dor Zohar <dor@emcie.co>

--- src/parlant/api/chat/src/components/message-fragment/message-fragment.tsx
+++ src/parlant/api/chat/src/components/message-fragment/message-fragment.tsx
@@ -12,15 +12,16 @@ import {twMerge} from 'tailwind-merge';
 // };
 
 const MessageFragment = ({fragment}: {fragment: {id: string; value: string}}) => {
+	const [id, value] = fragment?.value || ['', ''];
 	return (
 		<div className='group flex justify-between group min-h-[40px] bg-white hover:bg-[#FAFAFA]'>
 			<div className='group [word-break:break-word] w-full flex gap-[17px] [&:first-child]:rounded-t-[3px] items-start text-[#656565] py-[8px] ps-[15px] pe-[38px]'>
 				<img src='icons/puzzle.svg' alt='' className='mt-[4px] w-[16px] min-w-[16px]' />
-				<div className={twMerge('invisible', fragment?.value && 'visible')}>{fragment?.value || 'loading'}</div>
+				<div className={twMerge('invisible', value && 'visible')}>{value || 'loading'}</div>
 			</div>
 			<Tooltip value='Copy' side='top'>
 				<div
-					onClick={(e) => copy(fragment.id || '', e.currentTarget)}
+					onClick={(e) => copy(id || '', e.currentTarget)}
 					className='hidden me-[10px] mt-[6px] cursor-pointer size-[28px] group-hover:flex justify-center items-center bg-white hover:bg-[#F3F5F9] border border-[#EEEEEE] hover:border-[#E9EBEF] rounded-[6px]'>
 					<img src='icons/copy.svg' alt='' />
 				</div>
