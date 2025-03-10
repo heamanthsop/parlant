df7b09404d1ee93d9c64487fa571e914cf0e72d9
Dor Zohar <dor@emcie.co>
2025-03-10 16:29:43 +0000

    Change messages in inspection from sequence of MessageEventData to str
    Signed-off-by: Dor Zohar <dor@emcie.co>

--- tests/api/test_sessions.py
+++ tests/api/test_sessions.py
@@ -935,7 +935,7 @@ async def test_that_a_message_is_generated_using_the_active_nlp_service(
 
     assert message_generation_inspections[0]["generation"]["usage"]["input_tokens"] > 0
 
-    assert "Woof Woof" in message_generation_inspections[0]["messages"][0]["message"]
+    assert "Woof Woof" in message_generation_inspections[0]["messages"][0]
     assert message_generation_inspections[0]["generation"]["usage"]["output_tokens"] >= 2
 
 
@@ -1048,55 +1048,6 @@ async def test_that_an_agent_message_can_be_generated_from_utterance_requests(
     assert len(events) == 1
     assert events[0]["id"] == event["id"]
     assert "thinking" in events[0]["data"]["message"].lower()
-
-
-async def test_that_fragments_can_be_inspected(
-    async_client: httpx.AsyncClient,
-    container: Container,
-    strict_agent_id: AgentId,
-) -> None:
-    fragment_store = container[FragmentStore]
-
-    customer = await create_customer(
-        container=container,
-        name="John Smith",
-    )
-
-    session = await create_session(
-        container=container,
-        agent_id=strict_agent_id,
-        customer_id=customer.id,
-    )
-
-    fragment = await fragment_store.create_fragment(value="Hey lad!)", fields=[])
-
-    customer_event = await post_message(
-        container=container,
-        session_id=session.id,
-        message="Bobo!",
-        response_timeout=Timeout(60),
-    )
-
-    reply_event = await read_reply(
-        container=container,
-        session_id=session.id,
-        customer_event_offset=customer_event.offset,
-    )
-
-    trace = (
-        (await async_client.get(f"/sessions/{session.id}/events/{reply_event.id}"))
-        .raise_for_status()
-        .json()["trace"]
-    )
-
-    iterations = trace["message_generations"]
-    assert len(iterations) >= 1
-
-    "Hey lad!" in trace["message_generations"][0]["messages"][0]["message"]
-
-    assert trace["message_generations"][0]["messages"][0].get("fragments")
-    fragment_ids = trace["message_generations"][0]["messages"][0]["fragments"]
-    fragment.id in fragment_ids
 
 
 async def test_that_an_event_with_fragments_can_be_generated(
