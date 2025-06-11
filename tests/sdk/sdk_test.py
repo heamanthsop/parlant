50a586d0562cf0a4bad81e9c099ce69d82c1bc2f
Dor Zohar <dor@emcie.co>
2025-06-11 15:06:00 +0000

    Return container in sdk_tests
    Signed-off-by: Dor Zohar <dor@emcie.co>

--- tests/sdk/sdk_test.py
+++ tests/sdk/sdk_test.py
@@ -1,10 +1,8 @@
 from textwrap import dedent
 
-from parlant.client import AsyncParlantClient as Client
-
 import parlant.sdk as p
 
-from tests.sdk.utils import SDKTest, get_message
+from tests.sdk.utils import Context, SDKTest, get_message
 from tests.test_utilities import nlp_test
 
 
@@ -16,8 +14,8 @@ class Test_that_an_agent_can_be_created(SDKTest):
             composition_mode=p.CompositionMode.COMPOSITED_UTTERANCE,
         )
 
-    async def run(self, client: Client) -> None:
-        agents = await client.agents.list()
+    async def run(self, ctx: Context) -> None:
+        agents = await ctx.client.agents.list()
         assert agents[0].name == "Test Agent"
 
 
@@ -29,7 +27,7 @@ class Test_that_a_created_journey_is_followed(SDKTest):
             composition_mode=p.CompositionMode.COMPOSITED_UTTERANCE,
         )
 
-        await self.agent.create_journey(
+        self.journey = await self.agent.create_journey(
             title="Greeting the customer",
             conditions=["the customer greets you"],
             description=dedent("""\
@@ -37,20 +35,20 @@ class Test_that_a_created_journey_is_followed(SDKTest):
             """),
         )
 
-    async def run(self, client: Client) -> None:
-        session = await client.sessions.create(
+    async def run(self, ctx: Context) -> None:
+        session = await ctx.client.sessions.create(
             agent_id=self.agent.id,
             allow_greeting=False,
         )
 
-        event = await client.sessions.create_event(
+        event = await ctx.client.sessions.create_event(
             session_id=session.id,
             kind="message",
             source="customer",
             message="Hello there",
         )
 
-        agent_messages = await client.sessions.list_events(
+        agent_messages = await ctx.client.sessions.list_events(
             session_id=session.id,
             min_offset=event.offset,
             source="ai_agent",
--- tests/sdk/utils.py
+++ tests/sdk/utils.py
@@ -1,5 +1,6 @@
 import asyncio
-from typing import cast
+from dataclasses import dataclass
+from typing import Callable, cast
 
 from parlant.client import AsyncParlantClient as Client
 from parlant.client.types.event import Event as ClientEvent
@@ -20,6 +21,12 @@ def get_message(event: ClientEvent) -> str:
     raise ValueError("Event does not contain a message in its data.")
 
 
+@dataclass
+class Context:
+    client: Client
+    container: p.Container
+
+
 class SDKTest:
     async def test_run(self) -> None:
         port = get_random_port()
@@ -29,14 +36,16 @@ class SDKTest:
 
         try:
             await self._wait_for_startup(client)
-            await self.run(client)
+            await self.run(Context(client, self.get_container()))
         finally:
             server_task.cancel()
             await server_task
 
     async def _create_server_task(self, port: int) -> asyncio.Task[None]:
         async def server_task() -> None:
-            async with await self.create_server(port) as server:
+            server, self.get_container = await self.create_server(port)
+
+            async with server:
                 await self.setup(server)
 
         task = asyncio.create_task(server_task())
@@ -57,17 +66,21 @@ class SDKTest:
 
                 await asyncio.sleep(0.333)
 
-    async def create_server(self, port: int) -> p.Server:
+    async def create_server(self, port: int) -> tuple[p.Server, Callable[[], p.Container]]:
+        test_container: p.Container = p.Container()
+
         async def configure_container(container: p.Container) -> p.Container:
-            container[PerceivedPerformancePolicy] = NullPerceivedPerformancePolicy()
-            return container
+            nonlocal test_container
+            test_container = container.clone()
+            test_container[PerceivedPerformancePolicy] = NullPerceivedPerformancePolicy()
+            return test_container
 
         return p.Server(
             port=port,
             tool_service_port=get_random_port(),
             log_level=p.LogLevel.DEBUG,
             configure_container=configure_container,
-        )
+        ), lambda: test_container
 
     async def setup(self, server: p.Server) -> None: ...
-    async def run(self, client: Client) -> None: ...
+    async def run(self, ctx: Context) -> None: ...
