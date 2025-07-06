5ed7cdfd011acd77cbb1cc8f6982fc8a5fa9c14a
Yam Marcovitz <yam@emcie.co>
2025-07-06 14:30:36 +0000

    Add multiple retriever test
    Signed-off-by: Yam Marcovitz <yam@emcie.co>
    
    Signed-off-by: Dor Zohar <dor@emcie.co>

--- tests/sdk/test_retrievers.py
+++ tests/sdk/test_retrievers.py
@@ -41,3 +41,31 @@ class Test_that_a_custom_retriever_can_be_used_to_add_data_to_message_context(SD
             context=response,
             condition="It says that an orange  eggplant is a type of tomato",
         )
+
+
+class Test_that_multiple_custom_retrievers_can_be_used_to_add_data_to_message_context(SDKTest):
+    async def setup(self, server: p.Server) -> None:
+        self.agent = await server.create_agent(
+            name="Rel Agent",
+            description="Agent for guideline relationships",
+        )
+
+        async def custom_retriever_1(ctx: p.RetrieverContext) -> str:
+            return "An orange eggplant is actually a special type of tomato"
+
+        async def custom_retriever_2(ctx: p.RetrieverContext) -> str:
+            return "Parla loves orange eggplants"
+
+        await self.agent.attach_retriever(custom_retriever_1)
+        await self.agent.attach_retriever(custom_retriever_2)
+
+    async def run(self, ctx: Context) -> None:
+        response = await ctx.send_and_receive(
+            customer_message="What's the name of he/she who is known to love tomatoes?",
+            recipient=self.agent,
+        )
+
+        assert nlp_test(
+            context=response,
+            condition="It mentions the name Parla",
+        )
